65bb98528fb3f35c949f5f7c5c9dbc557259880e
OlyMachete <fabio.oliva1986@gmail.com>
2012-11-13 22:21:43 +0100

    gestione errori esercizio 4

--- Esercizio4_blog/blog.rb
+++ Esercizio4_blog/blog.rb
@@ -20,9 +20,12 @@ DataMapper.finalize
 home page  con slim e visualizzazione ultimi 5 post in ordine di data descrescente
 =end
 get '/' do
-#	@posts = Post.all
-	@posts = Post.all(:order => [ :inserted_at.desc ], :limit => 5)
-	slim :index2
+  @posts = Post.all(:order => [ :inserted_at.desc ], :limit => 5)
+	if @posts.empty?
+	  @error = "There are no post! Create Yours!"
+   slim :error
+	end
+   slim :index2
 end
 
 =begin
@@ -38,8 +41,12 @@ ambaradam per fargli mettere la data di inserimento post
 post '/newpost' do
   blogpost = Post.create params[:post]
   blogpost.inserted_at = blogpost.inserted_at.nil? ? Time.now : nil
-  blogpost.save
+  if  blogpost.save
   redirect to('/')
+  else
+   @error = "Error inserting the post"
+   slim :error
+ end
 end
 
 =begin
@@ -48,7 +55,12 @@ visualizzazione dettaglio post
 
 get '/post/:id' do
  @detail = Post.get(params[:id])
+ if @detail
  slim :detail 
+ else
+   @error = "The post ##{params[:id]} does not exist"
+   slim :error
+ end
 end
 
 =begin
@@ -56,8 +68,13 @@ delete di un post
 =end
 
 delete '/post/:id' do
-  Post.get(params[:id]).destroy
+  @destroy = Post.get(params[:id]).destroy
+  if @destroy
   redirect to('/')
+  else
+    @error = "Errore deleting the post ##{params[:id]}"
+   slim :error
+ end
 end
 
 =begin
